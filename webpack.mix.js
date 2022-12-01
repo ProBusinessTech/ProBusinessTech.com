ae93326abb4ef4d6cadc84ba62b29f493c18f030
David Walker <dave@sitecoredave.com>
2022-11-30 20:15:39 -0600

    app.scss path

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require("laravel-mix");
 
 mix.disableNotifications()
 
-mix.js("src/js/app.js", "dist/").sass("src/scss/app.scss", "dist/");
+mix.js("src/js/app.js", "dist/").sass("src/assets/styles/app.scss", "dist/");
 
 // Autoprefixer is enabled by default, see:
 // https://laravel-mix.com/docs/6.0/autoprefixer
